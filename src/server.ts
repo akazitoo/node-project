5ec9dfb40c3e0e9185699415215fd7564229e646
akazitoo <akds@cesar.school>
2024-04-05 14:27:18 -0300

    added foreing key, routes setted and added, utils and libs created

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,20 @@
-import fastfy from "fastify";
-import {z} from "zod";
-import { PrismaClient } from "@prisma/client";
+import fastify from "fastify";
+import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
+import { createEvent } from "./routes/create-event";
+import { registerForEvent } from "./routes/register-for-event";
+import { getEvent } from "./routes/get-event";
+import { getAttendeeBadge } from "./routes/get-attendee-badge";
 
-const app = fastfy();
 
-const prisma = new PrismaClient({
-        log: ['query'],
-})
-app.post('/events', async (request, reply) => {
-        const createEventSchema = z.object({
-                title: z.string().min(4),
-                details: z.string().nullable(),
-                maximumAttendees: z.number().int().positive().nullable(),
-        })
-        const data = createEventSchema.parse(request.body)
-        const event = await prisma.event.create({
-                data: {
-                        title: data.title,
-                        details: data.details,
-                        maximumattendees: data.maximumAttendees,
-                        slug: new Date().toISOString(),
-                },
-        })
-        return reply.status(201).send({eventId: event.id})
-})
+export const app = fastify();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createEvent);
+app.register(registerForEvent);
+app.register(getEvent);
+app.register(getAttendeeBadge);
 
 app.listen({port : 3333}).then(() =>{
         console.log('HTTP Server Running')
